df73172f4de98aee0d8abf1d2aa69ac1695d2949
Arushi Aggarwal <arushi3.14@outlook.com>
2024-11-04 15:37:09 -0500

    basic window running

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from kivy.app import App
 from kivy.uix.label import Label
 
 class MyApp(App):
-        def buil(self):
+        def build(self):
             return Label(text="This is a label")
         
 if __name__ == "__main__":
-      MyApp().run
+      MyApp().run()
