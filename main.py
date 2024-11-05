b114d2ebf15ef08ad7126cae7ab97d3cb2c1bc88
Arushi Aggarwal <arushi3.14@outlook.com>
2024-11-05 15:48:28 -0500

    setup for create event page

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.config import Config
+
+from create_event import CreateEvent
+
+# Set the window size
+Config.set('graphics', 'width', '400')
+Config.set('graphics', 'height', '720')
 
 class MyApp(App):
         def build(self):
-            return Label(text="This is a label")
+            return CreateEvent()
         
 if __name__ == "__main__":
       MyApp().run()
