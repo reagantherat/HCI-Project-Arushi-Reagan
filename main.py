f2972beff5967549f5368c0a2455a2f25b7bae57
Arushi Aggarwal <arushi3.14@outlook.com>
2024-11-08 13:32:13 -0500

    before deleting popup format

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from kivy.uix.popup import Popup
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '720')
 
-#NAVIGATION
+# NAVIGATION
 class MainWindow(Screen):
     def go_create_event(self):
         sm.current = "create_event"
@@ -18,12 +18,13 @@ class MainWindow(Screen):
 class CreateEvent(Screen):
     def go_back(self):
         sm.current = "main"
-    def month_popup(self):
-        show_month_popup()
+    def month_handler(val):
+        print(val)
         ### TODO: set the value of the selection from the date to the actual value from the popup 
         # - will need to go to show_month_popup and get a returned value from there 
         # self.ids.month_select.text = value # (from show_month_popup)
-        
+    def month_popup(self):
+        show_month_popup()
     def day_popup(self):
         show_day_popup()
         ### TODO: see above
@@ -43,33 +44,44 @@ for screen in screens:
 
 sm.current = "main"
 
-#POPUPS
+# POPUPS
 class MonthPopup(FloatLayout):
-    pass
-def show_month_popup():
-    show = MonthPopup()
+    def month_select(self, val):
+        CreateEvent.month_handler(val)
+    def close_popup(self):
+        monthPopup.dismiss()
+    
+show = MonthPopup()
+monthPopup = Popup(title="Choose a month", content=show, size_hint=(None, None), size=(375, 500))
 
-    monthPopup = Popup(title="Choose a month", content=show, size_hint=(None, None), size=(375, 500))
+def show_month_popup():
     monthPopup.open()
-    ### TODO: return the date returned by the button
+
 
 class DayPopup(FloatLayout):
-    pass
-def show_day_popup():
-    show = DayPopup()
+    def day_select(self, val):
+        print(val)
+    def close_popup(self):
+        dayPopup.dismiss()
 
-    dayPopup = Popup(title="Choose a day", content=show, size_hint=(None, None), size=(375, 500))
+show2 = DayPopup()
+dayPopup = Popup(title="Choose a day", content=show2, size_hint=(None, None), size=(375, 500))
+
+def show_day_popup():
     dayPopup.open()
-    ### TODO: see above
+
 
 class YearPopup(FloatLayout):
-    pass
-def show_year_popup():
-    show = YearPopup()
+    def year_select(self, val):
+        print(val)
+    def close_popup(self):
+        yearPopup.dismiss()
 
-    yearPopup = Popup(title="Choose a year", content=show, size_hint=(None, None), size=(375, 500))
+show3 = YearPopup()
+yearPopup = Popup(title="Choose a year", content=show3, size_hint=(None, None), size=(375, 500))
+
+def show_year_popup():
     yearPopup.open()
-    ### TODO: see above
 
 class MyMainApp(App):
     def build(self):
