aea4c3e18c492c07ff9083dd0192e16c3c4858b1
Arushi Aggarwal <arushi3.14@outlook.com>
2024-11-08 13:56:18 -0500

    date and time selection with passed vals

--- main.py
+++ main.py
@@ -18,19 +18,25 @@ class MainWindow(Screen):
 class CreateEvent(Screen):
     def go_back(self):
         sm.current = "main"
-    def month_handler(val):
-        print(val)
-        ### TODO: set the value of the selection from the date to the actual value from the popup 
-        # - will need to go to show_month_popup and get a returned value from there 
-        # self.ids.month_select.text = value # (from show_month_popup)
-    def month_popup(self):
-        show_month_popup()
-    def day_popup(self):
-        show_day_popup()
-        ### TODO: see above
-    def year_popup(self):
-        show_year_popup()
-        ### TODO: see above
+    def month_clicked(self, val):
+        print("month: " + val)
+    def day_clicked(self, val):
+        print("day: " + val)
+    def year_clicked(self, val):
+        print("year: " + val)
+    def s_hour_clicked(self, val):
+        print("s hour: " + val)
+    def s_minute_clicked(self, val):
+        print("s minute: " + val)
+    def e_hour_clicked(self, val):
+        print("e hour: " + val)
+    def e_minute_clicked(self, val):
+        print("e minute: " + val)
+    def am_pm_clicked(self, instance, val, am_pm):
+        if (val):
+            print(am_pm)
+
+
 
 class WindowManager(ScreenManager):
     pass
@@ -43,45 +49,6 @@ for screen in screens:
     sm.add_widget(screen)
 
 sm.current = "main"
-
-# POPUPS
-class MonthPopup(FloatLayout):
-    def month_select(self, val):
-        CreateEvent.month_handler(val)
-    def close_popup(self):
-        monthPopup.dismiss()
-    
-show = MonthPopup()
-monthPopup = Popup(title="Choose a month", content=show, size_hint=(None, None), size=(375, 500))
-
-def show_month_popup():
-    monthPopup.open()
-
-
-class DayPopup(FloatLayout):
-    def day_select(self, val):
-        print(val)
-    def close_popup(self):
-        dayPopup.dismiss()
-
-show2 = DayPopup()
-dayPopup = Popup(title="Choose a day", content=show2, size_hint=(None, None), size=(375, 500))
-
-def show_day_popup():
-    dayPopup.open()
-
-
-class YearPopup(FloatLayout):
-    def year_select(self, val):
-        print(val)
-    def close_popup(self):
-        yearPopup.dismiss()
-
-show3 = YearPopup()
-yearPopup = Popup(title="Choose a year", content=show3, size_hint=(None, None), size=(375, 500))
-
-def show_year_popup():
-    yearPopup.open()
 
 class MyMainApp(App):
     def build(self):
