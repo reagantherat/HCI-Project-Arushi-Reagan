80eeee6bc205489a6853f4d35644381e27143372
reagantherat <white.rea@northeastern.edu>
2024-11-09 15:28:28 -0500

    Updated Filter and Home Screens

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
+from kivy.uix.switch import Switch
+from kivy.uix.checkbox import CheckBox 
 
 # Set the window size
 Config.set('graphics', 'width', '400')
@@ -14,6 +16,8 @@ Config.set('graphics', 'height', '720')
 class MainWindow(Screen):
     def go_create_event(self):
         sm.current = "create_event"
+    def go_event_filters(self):
+        sm.current = "event_filters"
 
 class CreateEvent(Screen):
     def go_back(self):
@@ -36,6 +40,36 @@ class CreateEvent(Screen):
         if (val):
             print(am_pm)
 
+class EventFilter(Screen):
+    def go_back(self):
+        sm.current = "main"
+    def month_clicked(self, val):
+        print("month: " + val)
+    def day_clicked(self, val):
+        print("day: " + val)
+    def year_clicked(self, val):
+        print("year: " + val)
+    def s_hour_clicked(self, val):
+        print("s hour: " + val)
+    def s_minute_clicked(self, val):
+        print("s minute: " + val)
+    def e_hour_clicked(self, val):
+        print("e hour: " + val)
+    def e_minute_clicked(self, val):
+        print("e minute: " + val)
+    def am_pm_clicked(self, instance, val, am_pm):
+        if (val):
+            print(am_pm)
+    def switch_callback(self, switchObject, switchValue):
+        if(switchValue):
+            print('Switch is ON:):):)')
+        else:
+            print('Switch is OFF:(:(:(')
+    def checkbox_click(self, instance, value):
+        if value is True:
+            print("Checkbox Checked")
+        else:
+            print("Checkbox Unchecked")
 
 
 class WindowManager(ScreenManager):
@@ -44,7 +78,7 @@ class WindowManager(ScreenManager):
 kv = Builder.load_file("my.kv")
 sm = WindowManager()
 
-screens = [MainWindow(name="main"), CreateEvent(name="create_event")]
+screens = [MainWindow(name="main"), CreateEvent(name="create_event"), EventFilter(name="event_filters")]
 for screen in screens:
     sm.add_widget(screen)
 
